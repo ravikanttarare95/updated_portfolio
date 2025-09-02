76169b6d8569a1e68e80a48da0fe824c4024cb62
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-02 09:57:03 +0530

    Project configs

---
+++ src/configs/Projects.js
@@ -0,0 +1,31 @@
+import { GrTechnology } from "react-icons/gr";
+
+const PROJECTS = [
+  {
+    title: "moneyBook",
+    desc: "moneyBook helps you take control of your finances by tracking your income and expenses all in one place.",
+    features: [
+      "Track all daily income and expenses in one place.",
+      "Category-wise income and expense management.",
+      "Monthly expense limit with overspending alerts.",
+    ],
+    technologies: ["HTML", "CSS", "JavaScript"],
+    gitHubLink: "https://github.com/ravikanttarare95/money-book.git",
+    liveLink: "https://github.com/ravikanttarare95/money-book.git",
+  },
+
+  {
+    title: "Swad",
+    desc: "Experience the authentic taste of India with our delicious, home-style meals. Fresh ingredients, traditional recipes, and a warm ambiance await you!",
+    features: [
+      "Browse Menu Items.",
+      "Add Menu Items to cart.",
+      "Increase/Decrease quantity.",
+      "Total price calculation.",
+    ],
+    technologies: ["React", "Node.js", "Tailwindcss"],
+    gitHubLink: "https://github.com/ravikanttarare95/swad-tasty-food.git",
+    liveLink: "https://swad-the-taste.netlify.app",
+  },
+];
+export default PROJECTS;
