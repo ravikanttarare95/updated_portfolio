553068ef607608b7a46679670dc31b48fe6231e6
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-10-15 23:03:59 +0530

    changed project config

--- src/configs/Projects.js
+++ src/configs/Projects.js
@@ -1,38 +1,40 @@
-import moneyBookImg from "./../assets/money-book.png";
-import swadImg from "./../assets/swad.png";
+import PocketPilotImg from "./../assets/pocket-pilot.png";
+import BloGridImg from "./../assets/blogrid.png";
 
 const PROJECTS_INFO = [
   {
     id: 1,
-    projectPhoto: moneyBookImg,
-    projectTitle: "moneyBook",
+    projectPhoto: BloGridImg,
+    projectTitle: "BloGrid",
     projectDesc:
-      "moneyBook helps you take control of your finances by tracking your income and expenses all in one place.",
+      "BloGrid is a blogging app to share your stories, ideas, and experiences with the world.",
+    features: [
+      "User authentication & login with Google.",
+      "Create, edit, and post blogs.",
+      "Users can comment, like, and mark blogs as favourites.",
+      "Image upload with drag & drop using imagekit.io.",
+      "API caching for faster performance.",
+      "Data encryption for security.",
+    ],
+    technologies: ["React", "Tailwindcss", "Node.js", "Express.js", "MondoDB"],
+    gitHubLink: "https://github.com/ravikanttarare95/blogrid.git",
+    liveLink: "https://blogrid-ui.onrender.com",
+  },
+  {
+    id: 2,
+    projectPhoto: PocketPilotImg,
+    projectTitle: "Pocket Pilot",
+    projectDesc:
+      "Pocket Pilot is a finance management app where users efficiently track and manage their personal finances, including income, expenses, budgets.",
     features: [
       "Track all daily income and expenses in one place.",
       "Category-wise income and expense management with pie chart.",
       "Monthly expense limit with overspending alerts.",
       "View full transaction history.",
     ],
-    technologies: ["HTML", "CSS", "JavaScript"],
-    gitHubLink: "https://github.com/ravikanttarare95/money-book.git",
-    liveLink: "https://money-book-ruby.vercel.app",
-  },
-
-  {
-    id: 2,
-    projectPhoto: swadImg,
-    projectTitle: "Swad",
-    projectDesc:
-      "Experience the authentic taste of India with our delicious, home-style meals. Fresh ingredients, traditional recipes, and a warm ambiance await you!",
-    features: [
-      "Implement Form Validation for Reviews.",
-      "Increase/Decrease item's quantity.",
-      "Total price calculation.",
-    ],
-    technologies: ["React", "Node.js", "Tailwindcss"],
-    gitHubLink: "https://github.com/ravikanttarare95/swad-tasty-food.git",
-    liveLink: "https://swad-the-taste.netlify.app",
+    technologies: ["React", "Tailwindcss", "Node.js", "Express.js", "MondoDB"],
+    gitHubLink: "https://github.com/ravikanttarare95/pocket-pilot.git",
+    liveLink: "https://pocket-pilot.onrender.com",
   },
 ];
 export default PROJECTS_INFO;
