1f291b08908ce9081fc34a0a1a87a961d7f1f000
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-11-07 12:17:12 +0530

    Changed Img

--- src/configs/Projects.js
+++ src/configs/Projects.js
@@ -4,24 +4,6 @@ import BloGridImg from "./../assets/blogrid.png";
 const PROJECTS_INFO = [
   {
     id: 1,
-    projectPhoto: BloGridImg,
-    projectTitle: "BloGrid",
-    projectDesc:
-      "BloGrid is a blogging app to share your stories, ideas, and experiences with the world.",
-    features: [
-      "User authentication & login with Google.",
-      "Create, edit, and post blogs.",
-      "Users can comment, like, and mark blogs as favourites.",
-      "Image upload with drag & drop using imagekit.io.",
-      "API caching for faster performance.",
-      "Data encryption for security.",
-    ],
-    technologies: ["React", "Tailwindcss", "Node.js", "Express.js", "MondoDB"],
-    gitHubLink: "https://github.com/ravikanttarare95/blogrid.git",
-    liveLink: "https://blogrid-ui.onrender.com",
-  },
-  {
-    id: 2,
     projectPhoto: PocketPilotImg,
     projectTitle: "Pocket Pilot",
     projectDesc:
@@ -37,6 +19,24 @@ const PROJECTS_INFO = [
     technologies: ["React", "Tailwindcss", "Node.js", "Express.js", "MondoDB"],
     gitHubLink: "https://github.com/ravikanttarare95/pocket-pilot.git",
     liveLink: "https://pocket-pilot.onrender.com",
+  },
+  {
+    id: 2,
+    projectPhoto: BloGridImg,
+    projectTitle: "BloGrid",
+    projectDesc:
+      "BloGrid is a blogging app to share your stories, ideas, and experiences with the world.",
+    features: [
+      "User authentication & login with Google.",
+      "Create, edit, and post blogs.",
+      "Users can comment, like, and mark blogs as favourites.",
+      "Image upload with drag & drop using imagekit.io.",
+      "API caching for faster performance.",
+      "Data encryption for security.",
+    ],
+    technologies: ["React", "Tailwindcss", "Node.js", "Express.js", "MondoDB"],
+    gitHubLink: "https://github.com/ravikanttarare95/blogrid.git",
+    liveLink: "https://blogrid-ui.onrender.com",
   },
 ];
 export default PROJECTS_INFO;
